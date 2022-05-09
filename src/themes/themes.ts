362b4603b7bb4b257df8011ef50bbfc4fc0512af
Ebuka Odini <ebukaodini@gmail.com>
2022-05-09 16:31:28 +0100

    refactor: updated the themes

--- src/themes/themes.ts
+++ src/themes/themes.ts
@@ -5,17 +5,27 @@ declare module 'styled-components' {
 export interface ITheme {
   primary: string
   active: string
-  secondary: string
+  alt: string
   light: string
+  altLight: string
   dark: string
   grey: string
+  neutral: string
+  success: string
+  warning: string
+  danger: string
 }
 
 export const Theme: ITheme = {
   primary: '#F68B1E',
   active: '#D87109',
-  secondary: '#0875C4',
+  alt: '#0875C4',
   light: '#FEF2E6',
+  altLight: '#E6F0FE',
   dark: '#311A02',
-  grey: '#9C8A78'
+  grey: '#B1A395',
+  neutral: '#F3F2F2',
+  success: '#3E8D63',
+  warning: '#FFC266',
+  danger: '#F02517',
 }
