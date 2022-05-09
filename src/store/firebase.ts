38ea6bee3db125cbd0a27fc9230ae4dce6d6f563
Ebuka Odini <ebukaodini@gmail.com>
2022-05-09 02:20:36 +0100

    feat: integrated firebase auth and setup database config

--- src/store/firebase.ts
+++ src/store/firebase.ts
@@ -1,5 +1,6 @@
 import { initializeApp } from 'firebase/app';
 import { getDatabase } from 'firebase/database'
+import { getAuth } from 'firebase/auth'
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_APIKEY,
@@ -13,4 +14,5 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig)
+export const auth = getAuth(app);
 export const database = getDatabase(app)
