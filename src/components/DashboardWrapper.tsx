7adc8d0ffa6c7b223fbe45bde668719d94374450
Ebuka Odini <ebukaodini@gmail.com>
2022-05-10 14:11:38 +0100

    refactor: implemented role based access authorization to the admin dashboard

--- src/components/DashboardWrapper.tsx
+++ src/components/DashboardWrapper.tsx
@@ -1,4 +1,7 @@
+import { useEffect } from "react"
+import { useHistory } from "react-router-dom"
 import styled from "styled-components"
+import { useAuthStore, useModalStore } from "../store"
 import { PageWrapper, Header } from "./"
 import { DashboardNav } from "./DashboardNav"
 
@@ -11,13 +14,24 @@ type props = {
 
 export const DashboardWrapper: React.FC<props> = ({ children }) => {
 
+  const { user } = useAuthStore()
+  const { replace } = useHistory()
+  const { toast } = useModalStore()
+
+  useEffect(() => {
+    if (user?.role === 'user') {
+      toast('Unauthorized access', 'danger')
+      replace('/')
+    }
+  }, [replace, toast, user?.role])
+
   return (
     <PageWrapper>
       <>
         <Header />
         <div className="container-lg text-dark py-lg-3">
 
-          <Body className="d-flex">
+          <Body className="d-flex gap-lg-3">
             <DashboardNav />
 
             <Content className="p-lg-3 w-100">
