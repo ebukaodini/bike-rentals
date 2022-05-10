17d06b957810654ed28e30a2cd634fba9ad6ba5e
Ebuka Odini <ebukaodini@gmail.com>
2022-05-10 14:10:09 +0100

    refactor: updated indexes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,9 @@
 export { Register } from './Register'
 export { Login } from './Login'
 export { Store } from './Store'
-export { Dashboard } from './Dashboard'
+export { Error } from './Error'
+
+export { Dashboard } from './admin/Dashboard'
+export { Users } from './admin/Users'
+export { Managers } from './admin/Managers'
+export { Bikes } from './admin/Bikes'
