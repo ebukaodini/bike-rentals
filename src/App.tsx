a96d3eaa9ee2031b27da6c08b72d8236901fcaad
Ebuka Odini <ebukaodini@gmail.com>
2022-05-12 10:41:18 +0100

    feat: added routes and implemented boot

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,37 @@
+import { useEffect } from 'react';
 import { Switch, Route } from 'react-router-dom'
-import { Dashboard, Login, Register, Store } from './pages'
+import { ConfirmModal, DynamicModal, LoadingModal, ToastWrapper } from './components';
+import { Bikes, Dashboard, Error, Login, Managers, Register, Reservations, Store, Users } from './pages'
+import { useBikeStore, useReservationStore } from './store';
 
 function App() {
+
+  const { getBikes } = useBikeStore()
+  const { getReservations } = useReservationStore()
+
+  useEffect(() => {
+    getBikes()
+    getReservations()
+  }, [getBikes, getReservations])
+
   return (
-    <Switch>
-      <Route exact path='/' component={Store} />
-      <Route exact path='/register' component={Register} />
-      <Route exact path='/login' component={Login} />
-      <Route exact path='/dashboard' component={Dashboard} />
-    </Switch>
+    <>
+      <ToastWrapper />
+      <ConfirmModal />
+      <DynamicModal />
+      <LoadingModal />
+      <Switch>
+        <Route exact path='/' component={Store} />
+        <Route exact path='/register' component={Register} />
+        <Route exact path='/login' component={Login} />
+        <Route exact path='/dashboard' component={Dashboard} />
+        <Route exact path='/dashboard/users' component={Users} />
+        <Route exact path='/dashboard/managers' component={Managers} />
+        <Route exact path='/dashboard/bikes' component={Bikes} />
+        <Route exact path='/dashboard/reservations' component={Reservations} />
+        <Route component={Error} />
+      </Switch>
+    </>
   );
 }
 
