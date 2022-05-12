45fd594016b1a033ca56ae16e3381c2de8b9dc06
Ebuka Odini <ebukaodini@gmail.com>
2022-05-12 10:35:05 +0100

    feat: added reservations page

--- src/store/reservation.ts
+++ src/store/reservation.ts
@@ -1,15 +1,17 @@
+import { onValue, ref, set as dbSet, update } from "firebase/database";
 import create, { State } from "zustand";
 import { persist } from "zustand/middleware";
 import { Bike } from "./bike";
+import { database } from "./firebase";
 import { User } from "./user";
 
 export type Reservation = {
   id: number
   bike: Bike['id']
   user: User['id']
-  reservedFrom: Date
-  reservedTo: Date
-  timestamp: Date
+  reservedFrom: string
+  reservedTo: string
+  timestamp: number
   isActive: boolean
 }
 
@@ -18,14 +20,42 @@ interface ReservationState extends State {
 }
 
 interface ReservationMethods extends State {
-  getReservations: () => Promise<any>
+  restoreDefault: () => void
+  getReservations: () => Promise<void>
+  addReservation: (
+    reservation: Reservation
+  ) => Promise<any>
 }
 
+const path = 'reservations/'
 export const useReservationStore = create<ReservationState & ReservationMethods>(
-  persist((get, set) => ({
+  persist((set, get) => ({
     reservations: [],
+    restoreDefault: () => {
+      set({
+        reservations: [],
+      })
+    },
     getReservations: async () => {
-      return []
+      const usersRef = ref(database, path)
+      onValue(usersRef, (snapshot) => {
+        if (snapshot.exists()) {
+          const data = Object.values(snapshot.val() as Reservation[])
+          set({
+            reservations: data
+          })
+        } else {
+          set({
+            reservations: []
+          })
+        }
+      })
+    },
+    addReservation: async (reservation) => {
+      return await dbSet(ref(database, path + reservation.id), reservation)
+        .then(async _=> {
+          return await get().getReservations()
+        })
     }
   }), {
     name: 'reservation'
